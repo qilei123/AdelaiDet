500366e0cf742999918285cc2ea7e27ec7aa5554
qilei123 <qileimail123@gmail.com>
2022-05-04 01:08:47 -0400

    test images

--- demo.sh
+++ demo.sh
@@ -1,3 +1,4 @@
 python demo/demo.py \
+    --config-file configs/FCOS-Detection/R_50_1x.yaml
     --input /data1/qilei_chen/DEVELOPMENTS/AdelaiDet/datasets/polyp2/adenoma \
     --output /data1/qilei_chen/DEVELOPMENTS/AdelaiDet/datasets/polyp2/result1
